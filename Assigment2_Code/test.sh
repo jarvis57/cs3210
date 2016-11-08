4100b2cd3cb955b71355ea7e9baaaaa79070d3e6
Jarvis Nguyen <nvdung149@gmail.com>
2016-11-08 15:45:16 +0800

    fix

--- Assigment2_Code/test.sh
+++ Assigment2_Code/test.sh
@@ -1,6 +1,6 @@
 for livePercent in 0 10 20 30 40 50 60 70 80 90 100
 do
-	for size in 3 5 1000 3000
+	for size in 5 3000
 	do
 		./genWorld $size $livePercent "../Gen-Data/random$size-$livePercent.txt"
 	done
