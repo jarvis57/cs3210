e03e54880aae7f60b873c3eb46236fe519635104
Jarvis Nguyen <nvdung149@gmail.com>
2016-11-08 19:43:22 +0800

    fix test.sh

--- Assigment2_Code/test.sh
+++ Assigment2_Code/test.sh
@@ -1,10 +1,11 @@
 mkdir ../Gen-Data
 for livePercent in 0 25 50 75 100
 do
-	for size in 5 3000
-	do
-		./genWorld $size $livePercent "../Gen-Data/random$size-$livePercent.txt"
-	done
+	./genWorld 3000 $livePercent "../Gen-Data/random3000-$livePercent.txt"
+done
+for livePercent in 0 5 10 15 20
+do
+	./genWorld 5 $livePercent "../Gen-Data/random5-$livePercent.txt"
 done
 
 make
@@ -15,7 +16,7 @@ for t in 1
 do
 	for w in 25 50 75 100
 	do
-		for p in 25 50 75 100
+		for p in 5 10 15 20
 		do
 			./SETL "../Gen-Data/random3000-$w.txt" 100 "../Gen-Data/random5-$p.txt" >test.ans
 			mpirun -machinefile machinefile.lab -rankfile rankfile.lab -np 12 ./SETL_par "../Gen-Data/random3000-$w.txt" 100 "../Gen-Data/random5-$p.txt" >test.out
